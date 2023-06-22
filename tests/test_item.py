a676479d04463c355069738049a3c551a83b3db1
vink77 <vink.av@mail.ru>
2023-06-23 00:19:46 +0700

    correct func

--- tests/test_item.py
+++ tests/test_item.py
@@ -3,21 +3,27 @@
 import pytest
 from src.item import Item
 
+
 @pytest.fixture
 def smart():
     return Item("Смартфон", 10000, 20)
+
 
 def get(smart):
     assert smart.name == "Смартфон"
     assert smart.price == 10000
     assert smart.quantity == 20
 
+
 def test_calculate_total_price(smart):
     assert smart.calculate_total_price() == 200000
+
 
 def test_apply_discount(smart):
     smart.pay_rate = 0.5
     assert smart.apply_discount() == 5000.0
 
+
+def test_apply_discount(smart):
     smart.pay_rate = 1.5
     assert smart.apply_discount() == 15000.0
