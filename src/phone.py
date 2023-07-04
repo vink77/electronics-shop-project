b893916a51cba8c38cc43c2d83601393a86cc1e3
vink77 <vink.av@mail.ru>
2023-07-04 23:53:24 +0700

    ADD file phone.py + func repr,str,init,add

--- src/phone.py
+++ src/phone.py
@@ -1,7 +1,7 @@
 from src.item import Item
 class Phone(Item):
     def __init__(self, name, price, quantity, number_of_sim) -> None:
-        Item.__init__(self, name, price, quantity)
+        super().__init__(name, price, quantity)
         self.number_of_sim = number_of_sim
 
 
