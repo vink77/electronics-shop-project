a11be7f21a4f66ee3463b69d87d352af16215825
vink77 <vink.av@mail.ru>
2023-07-05 08:04:49 +0700

    add setter2

--- src/phone.py
+++ src/phone.py
@@ -1,17 +1,8 @@
 from src.item import Item
 class Phone(Item):
     def __init__(self, name, price, quantity, number_of_sim: int) -> None:
-        print("Инициализация Phone")
         super().__init__(name, price, quantity)
-        print("Инициализация Phone1")
-
         self._number_of_sim = number_of_sim
-        print(self.number_of_sim)
-
-        print("Инициализация Phone2")
-
-        print(number_of_sim)
-
 
     def __repr__(self):
         result =f'{self.__class__.__name__}(\'{self.name}\', {self.price}, {self.quantity}, {self.number_of_sim})'
